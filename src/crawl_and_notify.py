a73f4a5d3e8b35ae00a85d5d5fb0d5f4fac31482
Feromrk <johannes.stark.js@gmail.com>
2022-10-07 22:35:52 +0200

    better style

--- src/crawl_and_notify.py
+++ src/crawl_and_notify.py
@@ -5,12 +5,20 @@ import yagmail
 import time
 import random
 from urllib.parse import urljoin
+import sys
+import re
+
+MIN_PYTHON = (3,6)
+assert sys.version_info >= MIN_PYTHON, "Python %s.%s or later is required.\n" % MIN_PYTHON
 
 class BetonInFormCrawler:
     def __init__(self):
         self.rootUrl = 'https://silke-hermes.de'
         self.silikonformenUrl = urljoin(self.rootUrl, '/shop/silikonformen')
         logging.debug("silikonformenUrl: '{}'".format(self.silikonformenUrl))
+
+    def getRootUrl(self):
+        return self.rootUrl
 
     def urlToSoup(self, url):
         return bs4.BeautifulSoup(requests.get(url).text, 'html.parser')
@@ -27,7 +35,7 @@ class BetonInFormCrawler:
         for p in banners:
             for content in p.contents:
                 if 'geschlossen' in content.lower():
-                    logging.info("Found Banner '{}'".format(content))
+                    logging.info(f"Found Banner '{content}'")
                     return False
         
         if(len(banners) == 0):
@@ -50,7 +58,7 @@ class BetonInFormCrawler:
             logging.error('no products found')
             return False
 
-        logging.debug('found {} products'.format(len(products)))
+        logging.debug(f"found {len(products)} products")
 
         productInStockTag = None
         for product in products:
@@ -60,12 +68,12 @@ class BetonInFormCrawler:
                     href = child['href']
                     break
             if not href:
-                logging.debug('strange: found no href for product {}'.format(product.string))
+                logging.debug(f"strange: found no href for product {product.string}")
                 continue
             productSoup = self.urlToSoup(href)
             variationsTag = productSoup.find(class_='variations_form')
             if variationsTag == None:
-                logging.debug('strange: found no variations for product {}'.format(getProductTitle(productSoup)))
+                logging.debug(f"strange: found no variations for product {getProductTitle(productSoup)}")
                 continue
             if 'in-stock' in variationsTag['data-product_variations']:
                 productInStockTag = productSoup
@@ -73,14 +81,15 @@ class BetonInFormCrawler:
         
         if not productInStockTag:
             productInStockTag = products[0]
-            logging.info("strange: no product found that is in stock; using '{}'".format(getProductTitle(productInStockTag)))
+            logging.warning(f"strange: no product found that is in stock; using '{getProductTitle(productInStockTag)}'")
         else:
-            logging.debug("product '{}' is in stock".format(getProductTitle(productInStockTag)))
+            logging.debug(f"product '{getProductTitle(productInStockTag)}' is in stock")
 
         warumKannIchNichtBestellenButtonTag = productInStockTag.find('a', 'single_add_to_cart_button')
         if not warumKannIchNichtBestellenButtonTag:
-            logging.error("'Warum kann ich nicht bestellen?' button not found")
-            return False
+            logging.info("'Warum kann ich nicht bestellen?' button not found; printing all add to cart buttons:")
+            logging.info(str(productInStockTag.find_all(class_=re.compile('add_to_cart'))))
+            return True
 
         if 'deaktiviert' in warumKannIchNichtBestellenButtonTag['href']:
             logging.info("found button '{}' in product '{}'".format(
@@ -102,12 +111,12 @@ if __name__ == '__main__':
         sleepSec = random.randint(600, 1800)
         
         if crawler.isShopOpenViaAddToCartButton():
-            logging.info("shop seems open, will notify")
-            yag = yagmail.SMTP("johannes.stark.js@gmail.com", oauth2_file="./lib/secret.json")
-            yag.send(subject="Beton In Form Shop ist offen", contents="Der Shop ist jetzt offen.", to="evaa.b@hotmail.com")
+            logging.info('shop seems open, will notify')
+            yag = yagmail.SMTP('johannes.stark.js@gmail.com', oauth2_file='./lib/secret.json')
+            yag.send(subject='BETON IN FORM Shop ist jetzt offen', contents=f"{crawler.getRootUrl()} ist jetzt offen.", to='evaa.b@hotmail.com')
             sleepSec += 3600
         else:
-            logging.info("shop seems closed")
+            logging.info('shop seems closed')
 
         logging.info("rechecking in {} minutes".format(int(sleepSec/60)))
         time.sleep(sleepSec)
